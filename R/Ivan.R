bb76d29957eb0c7d999ec0fa723031be02ce1d67
Danny Kaplan <kaplan@macalester.edu>
2020-05-04 12:59:45 -0500

    Added Ivan 2004 trajectory

---
+++ R/Ivan.R
@@ -0,0 +1,7 @@
+#' Track of Hurricane Ivan in Sept. 2004
+#'
+#' Data: https://www.nhc.noaa.gov/data/hurdat/hurdat2-1851-2019-042820.txt
+#' Format for data: https://www.nhc.noaa.gov/data/hurdat/hurdat2-format-nov2019.pdf
+
+#'
+"Ivan"
