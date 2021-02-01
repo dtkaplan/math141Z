c52e4c0cc47c364ff768d3ad29d854a5ef97066e
Danny Kaplan <kaplan@macalester.edu>
2021-02-01 17:28:58 -0600

    improved flow_field(), added iterate()

--- R/PP.R
+++ R/PP.R
@@ -2,27 +2,32 @@
 #'
 #' @param dynamics a list containing two one-sided formulas specifying
 #' the dynamics, e.g. `list(~ x + y, ~ y - x)`
+#' @param domain list of two vectors specifying domain of the initial
+#' points of each streamline Vector of length 2 specifying the domain
 #' @param n number of arrows to draw on a side. $n^2$ will be the number of arrows
 #' overall.
-#' @param pseudo Logical flag. Whether to position the arrows pseudo-randomly rather than
-#' on a strict grid.
+#'
+#' @examples
+#' streamlines(list(~ u+v, ~ u - v), stepsize=0.01, n=20)
 #'
 #' @export
-flow_field <- function(dynamics, xrange = 0:1, yrange=0:1,
-                       n = 10, pseudo=FALSE,
+streamlines <- function(dynamics, domain=list(x=c(0,1), y=c(0,1)),
+                       n = 10,
                        nsteps = 10, stepsize=0.05) {
   dynfuns <- make_dynamics(dynamics)
   library(magrittr)
-  if (!pseudo) {
-    xpts <- seq(min(xrange), max(xrange), length = n+2)
-    xpts <- xpts[-c(1, length(xpts))]
-    ypts <- seq(min(yrange), max(yrange), length = n+2)
-    ypts <- ypts[-c(1, length(ypts))]
-    Grid <- expand.grid(xpts, ypts)
-    names(Grid) <- c("x", "y")
-  } else {
-    # not implemented yet
-  }
+  xrange <- domain[[1]]
+  xstep <- 0.1*diff(xrange)/nsteps # for random displacement of starting points
+  yrange <- domain[[2]]
+  ystep <- 0.1*diff(yrange)/nsteps
+  xpts <- seq(min(xrange), max(xrange), length = n+2)
+  xpts <- xpts[-c(1, length(xpts))]
+  ypts <- seq(min(yrange), max(yrange), length = n+2)
+  ypts <- ypts[-c(1, length(ypts))]
+  Grid <- expand.grid(xpts, ypts)
+  names(Grid) <- c("x", "y")
+  Grid$x <-Grid$x + runif(nrow(Grid),min=-xstep, max=xstep)
+  Grid$y <-Grid$y + runif(nrow(Grid),min=-ystep, max=ystep)
 
   Flows <- list()
   for (point in 1:nrow(Grid)) {
@@ -37,26 +42,25 @@ flow_field <- function(dynamics, xrange = 0:1, yrange=0:1,
       y[k] <- y[k-1] + dy
     }
 
-    Flows[[point]] <- tibble::tibble(x=x, y=y, group=point)
+    Flows[[point]] <- tibble::tibble(x=x, y=y,
+                                     group=point,
+                                     alpha = 0.2+(1:nsteps)/(1.2*nsteps),
+                                     size = alpha + 0.6
+                                     )
   }
-  # Evaluate the dynamics on the grid
-  # dx <- dynfuns[[1]](Grid$x, Grid$y) # small step
-  # dy <- dynfuns[[2]](Grid$x, Grid$y)
-  # normalize <- max( max(abs(dx) + 1/n), max(abs(dy) + 1/n))
-  # arrow_lengthx <- abs(diff(xrange))/(n+2)
-  #
-  # Grid$dx <- dx * arrow_lengthx / normalize
-  # Grid$dy <- dy * arrow_lengthx / normalize
-  # Grid$endx <- Grid$x + Grid$dx
-  # Grid$endy <- Grid$y + Grid$dy
+  Paths <- dplyr::bind_rows(Flows)
+  # get the last point in the path
+  Last <- Paths %>% group_by(group) %>%
+    filter(row_number() == n())
 
-  ggformula::gf_path(y ~ x, data = dplyr::bind_rows(Flows),
-                     group = ~ group) %>%
-    ggformula::gf_point(y ~ x, data = Grid, inherit=FALSE)
-  # ggformula::gf_segment(y + endy ~ x + endx, data = Grid,
-  #            arrow =
-  #              grid::arrow(length = grid::unit(0.01, "snpc"))) +
-  #  ggplot2::labs(x = dynfuns[[3]][1], y = dynfuns[[3]][2])
+  ggformula::gf_path(y ~ x, data = Paths,
+                     lineend = "round",
+                     group = ~ group, color="black", alpha=~ alpha, size= ~ size) %>%
+    #ggformula::gf_point(y ~ x, data = Last, shape=23, fill="black", inherit=FALSE) %>%
+    ggformula::gf_labs(x = names(domain)[1], y = names(domain)[2]) %>%
+    gf_refine(scale_alpha_identity(),
+              scale_size_identity())
+
 }
 
 
@@ -86,6 +90,3 @@ make_dynamics <- function(dynamics) {
   return(list(dx, dy, vnames))
 }
 
-
-
-goo <- flow_field(list(~ u+v, ~ u - v))
---
+++ R/iterate.R
@@ -0,0 +1,41 @@
+#' Iterate a function on an initial condition
+#'
+#' Iterates a function a specified number of times on an initial condition.
+#'
+#' @details The function `f` can take one or more arguments. The first of these
+#' should represent the *state* of the dynamical system, e.g. x or x and y, etc.
+#' At the end of the argument list to `f` can come named parameters. The length of the
+#' initial condition `x0` must match the number of state arguments. Numerical values for
+#' parameters (if any) must be provided in the `...` slot.
+#'
+#' @param f a function of one or more state variables which returns a vector containing
+#' those same state variables. Parameters to `f()`, if any, should be named and at
+#' the end of the argument list.
+#' @param x0 a vector with the numerical initial condition. There should be 1 component
+#' in `x0` for each of the state variables in `f()`.
+#' @param n an integer specifying the number of iterations
+#' @param ... values for numerical parameters to the function `f()`
+#'
+#' @return A data frame with a column `.i` listing the iteration number
+#' and columns for each component of the initial condition. There will be n+1
+#' rows, the first for the initial condition and the remaining for the n iterations.
+#'
+#' @examples
+#' iterate(function(x, mu=3.5) mu*x*(1-x), .232, n=10, mu=4) # chaos
+#' iterate(function(x, y) c(x+y, x-y), c(1,1), n=10)
+#' iterate(function(x, y) c(x+y, x), c(0,1), n=10) # fibonacci
+#' @export
+iterate <- function(f=cos, x0=0, n=10, ...) {
+  dots <- list(...)
+  vnames <- names(formals(f))
+  if (is.null(vnames)) vnames <- "x"
+  if ((length(x0)+ length(dots)) != length(vnames))
+    stop("x0 must be a vector with one element for each argument to f.")
+  res <- matrix(0, nrow=n+1, ncol=length(x0))
+  res[1,] <- x0
+  for (k in 2:(n+1)) res[k,] <- do.call(f, c(as.list(res[k-1,]), dots))
+  out <- cbind(0:n, as.data.frame(res))
+  names(out) <- c(".i", setdiff(vnames, names(dots)))
+
+  out
+}
