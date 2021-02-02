dcf751cdbbbec6489aca11f7c7fd0128b4634513
Danny Kaplan <kaplan@macalester.edu>
2021-02-02 12:09:21 -0600

    better parameter handling in iterate()

--- R/iterate.R
+++ R/iterate.R
@@ -10,32 +10,32 @@
 #'
 #' @param f a function of one or more state variables which returns a vector containing
 #' those same state variables. Parameters to `f()`, if any, should be named and at
-#' the end of the argument list.
+#' the end of the argument list. State variables should **not** have default values.
 #' @param x0 a vector with the numerical initial condition. There should be 1 component
 #' in `x0` for each of the state variables in `f()`.
 #' @param n an integer specifying the number of iterations
-#' @param ... values for numerical parameters to the function `f()`
+#' @param fargs list containing values for numerical parameters to the function `f()`
 #'
 #' @return A data frame with a column `.i` listing the iteration number
 #' and columns for each component of the initial condition. There will be n+1
 #' rows, the first for the initial condition and the remaining for the n iterations.
 #'
 #' @examples
-#' iterate(function(x, mu=3.5) mu*x*(1-x), .232, n=10, mu=4) # chaos
+#' iterate(function(x, mu=3.5) mu*x*(1-x), .232, n=10, list(mu=4)) # chaos
 #' iterate(function(x, y) c(x+y, x-y), c(1,1), n=10)
 #' iterate(function(x, y) c(x+y, x), c(0,1), n=10) # fibonacci
 #' @export
-iterate <- function(f=cos, x0=0, n=10, ...) {
-  dots <- list(...)
+iterate <- function(f=cos, x0=0, n=10, fargs=list()) {
   vnames <- names(formals(f))
+  has_default <- sapply(formals(f), class) != "name"
   if (is.null(vnames)) vnames <- "x"
-  if ((length(x0)+ length(dots)) != length(vnames))
+  if ((length(x0)+ length(fargs)) < length(vnames) - sum(has_default))
     stop("x0 must be a vector with one element for each argument to f.")
   res <- matrix(0, nrow=n+1, ncol=length(x0))
   res[1,] <- x0
-  for (k in 2:(n+1)) res[k,] <- do.call(f, c(as.list(res[k-1,]), dots))
+  for (k in 2:(n+1)) res[k,] <- do.call(f, c(as.list(res[k-1,]), fargs))
   out <- cbind(0:n, as.data.frame(res))
-  names(out) <- c(".i", setdiff(vnames, names(dots)))
+  names(out) <- c(".i", setdiff(vnames, vnames[has_default]))
 
   out
 }
