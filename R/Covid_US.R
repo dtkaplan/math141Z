fc13feeb58e8a5c43e2dbe3e46b58665a7ccacb9
Danny Kaplan <kaplan@macalester.edu>
2021-08-19 10:50:15 -0600

    Adding up-to-date Covid-19 data

--- R/Covid_US.R
+++ R/Covid_US.R
@@ -1,3 +1,6 @@
 #' COVID data from the first half of the pandemic
 #'
+#' See Guidotti, E., Ardia, D., (2020), "COVID-19 Data Hub", Journal
+#' of Open Source Software 5(51):2376, doi: 10.21105/joss.02376.
+#'
 "Covid_US"
